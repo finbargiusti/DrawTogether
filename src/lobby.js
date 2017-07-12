abb25ff78de1105c1c4852ea380eb56c03705ed5
David Payr <dpayr@musica-viva.de>
2017-07-12 22:25:35 +0200

    Coded general connection handling

--- src/lobby.js
+++ src/lobby.js
@@ -1,15 +1,62 @@
+let controls = document.getElementById("controls");
 let createLobbyBtn = document.getElementById("createLobby");
+let submitIDBtn = document.getElementById("submitId");
+let lobbyIDInput = document.getElementById("lobbyId");
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
 createLobbyBtn.addEventListener("click", function() {
-    canvas.style.display = "block";
-    createLobbyBtn.style.display = "none";
+    if (connected && !connectingToLobby) {
+        connectingToLobby = true;
+        
+        socket.send(JSON.stringify({
+            type: "createLobby"
+        }));
+
+        socket.onmessage = lobbyJoinHandler;
+    }
 });
 
-ctx.beginPath();
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 400);
-ctx.stroke();
+submitIDBtn.addEventListener("click", function() {
+    if (connected && !connectingToLobby) {
+        connectingToLobby = true;
+        
+        socket.send(JSON.stringify({
+            type: "joinLobby",
+            requestedID: Number(lobbyIDInput.value);
+        }));
 
+        socket.onmessage = lobbyJoinHandler;
+    }
+});
+
+let connected = false;
+let lobbyID = null;
+let connectingToLobby = false;
 let socket = new WebSocket("ws://192.168.1.106:1337/");
+
+socket.onopen = function() {
+    connected = true;
+};
+
+socket.onclose = function() {
+    connected = false;
+};
+
+function joinLobby() {
+    controls.style.display = "none";
+    canvas.style.display = "block";
+}
+    
+function lobbyJoinHandler(event) {
+    let data = JSON.parse(event.data);
+    
+    if (data.type === "joinLobby") {
+        joinLobby();
+        lobbyID = data.lobbyID;
+    } else if (data.type === "incorrectID") {
+        alert("That lobby does not exist!");
+    }
+    
+    connectingToLobby = false;
+}
--- src/server.js
+++ src/server.js
@@ -5,13 +5,65 @@ connect().use(serveStatic(__dirname)).listen(8080, function(){
     console.log('Server running on 8080...');
 });
 
-
-
 // Web socket
 
 var ws = require("ws");
 var server = new ws.Server({port: 1337});
 
+var sockets = {};
+var currentSocketID = 0;
+var lobbies = [];
+
 server.on("connection", function(ws) {
-    console.log("dankity");
+    console.log("New web socket connection established! Dankity.");
+    
+    sockets[currentSocketID] = ws;
+    var thisID = currentSocketID;
+    ws.DTData = {};
+    
+    ws.on("message", function(message) {
+        var command = JSON.parse(message);
+        handleCommand(command);
+    });
+    
+    ws.on("close", function() {
+        detele sockets[thisID];
+    });
 });
+
+function Lobby(id) {
+    this.id = id;
+}
+
+function handleCommand(command, socket) {
+    if (command.type === "createLobby") {
+        var newLobbyID = 10000 + Math.floor(Math.random() * 90000);
+        
+        while (true) { // Checks if generated newLobbyID is duplicate
+            for (var i = 0; i < lobbies.length; i++) {
+                if (lobbies[i].id === newLobbyID) {
+                    newLobbyID = 10000 + Math.floor(Math.random() * 90000);
+                    break;
+                }
+            }
+            break;
+        }
+        
+        lobbies.push(new Lobby(newLobbyID));
+        socket.DTData.lobbyID = newLobbyID;
+        socket.send(JSON.stringify({
+            type: "joinLobby",
+            lobbyID: newLobbyID
+        }));
+    } else if (command.type === "connectToLobby") {
+        for (var i = 0; i < lobbies.length; i++) {
+            if (lobbies[i].id === command.requestedID) {
+                socket.DTData.lobbyID = newLobbyID;
+                socket.send(JSON.stringify({
+                    type: "joinLobby",
+                    lobbyID: lobbies[i].id
+                }));
+            }
+        }
+    }
+}
