44dc93bbe6d3c7d28ba0bccf9739adeb9bc34d31
finbargiusti <finbargiusti@gmail.com>
2017-07-13 15:23:33 +0200

    Added painting, colors and eraser

--- src/lobby.js
+++ src/lobby.js
@@ -1,8 +1,14 @@
 let controls = document.getElementById("controls");
 let createLobbyBtn = document.getElementById("createLobby");
+let writingUtensils = document.getElementById("writingUtensils");
+let colorTxtField = document.getElementById("colorPick");
 let submitIDBtn = document.getElementById("submitId");
 let lobbyIDInput = document.getElementById("lobbyId");
 let canvas = document.getElementById("canvas");
+let idDisplayP = document.getElementById("idDisplay");
+let pencilRadio = document.getElementById("pencil");
+let rubberRadio = document.getElementById("rubber");
+let brushRadio = document.getElementById("brush");
 let ctx = canvas.getContext("2d");
 
 let mouseX = 0, mouseY = 0, mousePressed = false;
@@ -14,20 +20,36 @@ let lastPosition = {
 document.addEventListener("mousemove", function(event) {
     mouseX = event.clientX;
     mouseY = event.clientY;
-    
+
     if (mousePressed) {
         let thisPosition = {
             x: mouseXElement(canvas),
             y: mouseYElement(canvas)
         };
-        
-        socket.send(JSON.stringify({
-            type: "drawLine",
-            from: lastPosition,
-            to: thisPosition
-        }));
+
+        if (pencilRadio.checked) {
+            socket.send(JSON.stringify({
+                type: "drawLine",
+                from: lastPosition,
+                to: thisPosition,
+                color: colorTxtField.value
+            }));
+        } else if (rubberRadio.checked) {
+            socket.send(JSON.stringify({
+                type: "eraseLine",
+                from: lastPosition,
+                to: thisPosition
+            }));
+        } else if (brushRadio.checked) {
+            socket.send(JSON.stringify({
+                type: "brushLine",
+                from: lastPosition,
+                to: thisPosition,
+                color: colorTxtField.value
+            }));
+        }
     }
-    
+
     lastPosition.x = mouseXElement(canvas), lastPosition.y = mouseYElement(canvas);
 });
 
@@ -80,7 +102,7 @@ submitIDBtn.addEventListener("click", function() {
 let connected = false;
 let lobbyID = null;
 let connectingToLobby = false;
-let socket = new WebSocket("ws://192.168.1.105:1337/");
+let socket = new WebSocket("ws://192.168.1.106:1337/");
 
 socket.onopen = function() {
     connected = true;
@@ -92,20 +114,45 @@ socket.onclose = function() {
 
 function joinLobby() {
     controls.style.display = "none";
-    canvas.style.display = "block";
+    writingUtensils.style.display = "block";
 }
 
 function serverCommandHandler(event) {
     let data = JSON.parse(event.data);
+    handleInstructions(data);
+}
 
-    console.log(event.data)
-    if (data.type === "drawLine") {
-        ctx.beginPath();
-        ctx.moveTo(data.from.x, data.from.y);
-        ctx.lineTo(data.to.x, data.to.y);
-        ctx.strokeStyle = "black";
-        ctx.stroke();
-    }
+function handleInstructions(arr) {
+  for (let i = 0; i < arr.length; ++i) {
+      let instruction = arr[i]
+      if (instruction.type === "drawLine") {
+          ctx.beginPath();
+          ctx.moveTo(instruction.from.x, instruction.from.y);
+          ctx.lineTo(instruction.to.x, instruction.to.y);
+          ctx.strokeStyle = instruction.color;
+          ctx.lineWidth = 4;
+          ctx.lineCap = "round";
+          ctx.stroke();
+      } else if (instruction.type === "eraseLine") {
+          ctx.beginPath();
+          ctx.moveTo(instruction.from.x, instruction.from.y);
+          ctx.lineTo(instruction.to.x, instruction.to.y);
+          ctx.strokeStyle = "ghostwhite";
+          ctx.lineWidth = 40;
+          ctx.lineCap = "round";
+          ctx.stroke();
+      } else if (instruction.type === "brushLine") {
+          let dist = Math.hypot(instruction.from.x - instruction.to.x,
+                                instruction.from.y - instruction.to.y);
+          ctx.beginPath();
+          ctx.moveTo(instruction.from.x, instruction.from.y);
+          ctx.lineTo(instruction.to.x, instruction.to.y);
+          ctx.strokeStyle = instruction.color;
+          ctx.lineWidth = Math.max(20 - dist, 1);
+          ctx.lineCap = "round";
+          ctx.stroke();
+      }
+  }
 }
 
 function lobbyJoinHandler(event) {
@@ -115,6 +162,8 @@ function lobbyJoinHandler(event) {
     if (data.type === "joinLobby") {
         joinLobby();
         lobbyID = data.lobbyID;
+        handleInstructions(data.instructions);
+        idDisplayP.innerHTML = "Lobby id (share with friends):"+lobbyID;
         socket.onmessage = serverCommandHandler;
     } else if (data.type === "incorrectID") {
         alert("That lobby does not exist!");
--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 8080...');
 });
 
 // Web socket
@@ -15,14 +14,12 @@ var currentSocketID = 0;
 var lobbies = [];
 
 server.on("connection", function(ws) {
-    console.log("New web socket connection established! Dankity.");
 
     sockets[currentSocketID] = ws;
     var thisID = currentSocketID;
     ws.DTData = {};
 
     ws.on("message", function(message) {
-        console.log(message)
         var command = JSON.parse(message);
         handleCommand(command, ws);
     });
@@ -35,6 +32,7 @@ server.on("connection", function(ws) {
 
 function Lobby(id) {
     this.id = id;
+    this.instructions = [];
 }
 
 function handleCommand(command, socket) {
@@ -42,20 +40,25 @@ function handleCommand(command, socket) {
         var newLobbyID = 10000 + Math.floor(Math.random() * 90000);
 
         while (true) { // Checks if generated newLobbyID is duplicate
+            var foundDuplicate = false;
+
             for (var i = 0; i < lobbies.length; i++) {
                 if (lobbies[i].id === newLobbyID) {
                     newLobbyID = 10000 + Math.floor(Math.random() * 90000);
+                    foundDuplicate = true;
                     break;
                 }
             }
-            break;
+
+            if (!foundDuplicate) break;
         }
 
         lobbies.push(new Lobby(newLobbyID));
         socket.DTData.lobbyID = newLobbyID;
         socket.send(JSON.stringify({
             type: "joinLobby",
-            lobbyID: newLobbyID
+            lobbyID: newLobbyID,
+            instructions: []
         }));
     } else if (command.type === "joinLobby") {
         for (var i = 0; i < lobbies.length; i++) {
@@ -63,23 +66,27 @@ function handleCommand(command, socket) {
                 socket.DTData.lobbyID = command.requestedID;
                 socket.send(JSON.stringify({
                     type: "joinLobby",
-                    lobbyID: lobbies[i].id
+                    lobbyID: lobbies[i].id,
+                    instructions: lobbies[i].instructions
                 }));
+
                 return;
             }
         }
         socket.send(JSON.stringify({
             type: "incorrectID"
         }));
-    } else if (command.type === "drawLine") {
+    } else if (command.type === "drawLine" || command.type === "eraseLine" || command.type === "brushLine") {
+        for (var i = 0; i < lobbies.length; ++i) {
+            if (lobbies[i].id === socket.DTData.lobbyID) {
+                lobbies[i].instructions.push(command)
+                console.log("lobby "+lobbies[i].id+" has a size of "+JSON.stringify(lobbies[i].instructions).length+"B")
+                break;
+            }
+        }
         for (var sckt in sockets) {
             if (sockets[sckt].DTData.lobbyID === socket.DTData.lobbyID) {
-                console.log(sockets[sckt].DTData.lobbyID);
-                sockets[sckt].send(JSON.stringify({
-                    type: "drawLine",
-                    from: command.from,
-                    to: command.to
-                }));
+                sockets[sckt].send(JSON.stringify([command]));
             }
         }
     }
