849710eee83db0680ad9b795de8f4330dab36f74
finbargiusti <finbargiusti@gmail.com>
2017-07-18 19:47:14 +0200

    Broke drag, adding color palette

--- src/drag.js
+++ src/drag.js
@@ -1,9 +1,14 @@
 window.onload = addListeners();
-var x_pos = 0,
+let x_pos = 0,
   y_pos = 0;
 
 function addListeners() {
-  document.getElementById('topbar').addEventListener('mousedown', mouseDown, false);
+  var elements = document.getElementsByClassName('topbar');
+  for (let i = 0; i < elements.length; ++i){
+      elements[i].addEventListener('mousedown', function(event) {
+        mouseDown(elements[i], event);
+      }, false);
+  }
   window.addEventListener('mouseup', mouseUp, false);
 }
 
@@ -11,16 +16,21 @@ function mouseUp() {
   window.removeEventListener('mousemove', divMove, true);
 }
 
-function mouseDown(e) {
-  var div = document.getElementById('optionsmenu');
-  x_pos = e.clientX - div.offsetLeft;
-  y_pos = e.clientY - div.offsetTop;
-  window.addEventListener('mousemove', divMove, true);
+console.bitch = console.log
+
+function mouseDown(elem, event) {
+  let div = elem.parentNode;
+  x_pos = event.clientX - div.offsetLeft;
+  y_pos = event.clientY - div.offsetTop;
+  window.addEventListener('mousemove', function() {
+    divMove(elem, event);
+  }, true);
 }
 
-function divMove(e) {
-  var div = document.getElementById('optionsmenu');
+function divMove(elem, event) {
+  console.log(event.clientY, y_pos)
+  let div = elem.parentNode;
   div.style.position = 'absolute';
-  div.style.top = (e.clientY - y_pos) + 'px';
-  div.style.left = (e.clientX - x_pos) + 'px';
+  div.style.top = (event.clientY - y_pos) + 'px';
+  div.style.left = (event.clientX - x_pos) + 'px';
 }
