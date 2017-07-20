469386173c666d70b0998e4aa411118f6a70bffd
David Payr <dpayr@musica-viva.de>
2017-07-20 18:49:40 +0200

    Cleaned up server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-// http
+// HTTP
 var connect = require('connect');
 var serveStatic = require('serve-static');
 
 connect().use(serveStatic(__dirname+"/client")).listen(8080, function(){});
 
-// Web socket
+// WebSocket
 
 var ws = require("ws");
 var server = new ws.Server({port: 1337});
@@ -44,19 +44,30 @@ function Lobby(id, width, height, bgColor) {
     this.bgColor = bgColor;
     this.instructions = [];
     this.palette = ["rgb(0,0,0)"];
+    
+    this.sendMsgToMembers = function(msg, excludedSocket) {
+        for (var sckt in sockets) {
+            if (sockets[sckt].DTData.lobbyID === this.id && ((excludedSocket) ? sockets[sckt] !== excludedSocket : true)) {
+                socket[sckt].send(msg);
+            }
+        }
+    }
+    
+    this.sendJoinInstruction = function(socket) {
+        socket.send(JSON.stringify([0, this.id, this.instructions, this.width, this.height, this.bgColor, this.palette]));
+    }
 }
-
-function sendJoinLobbyInstruction(socket, lobby) {
-    socket.send(JSON.stringify([0, lobby.id, lobby.instructions, lobby.width,
-                               lobby.height, lobby.bgColor, lobby.palette]));
+Lobby.prototype.getLobbyByID = function(ID) {
+    for (var i = 0; i < lobbies.length; i++) {
+        if (lobbies[i].id === ID) {
+            return lobbies[i];
+        }
+    }
+    return null;
 }
 
 function onPlayerDisconnect(socket) {
-    for (var sckt in sockets) {
-        if (sockets[sckt].DTData.lobbyID === socket.DTData.lobbyID && sockets[sckt] !== socket) {
-            sockets[sckt].send(JSON.stringify([2, [socket.DTData.id]]));
-        }
-    }
+    Lobby.getLobbyByID(socket.DTData.lobbyID).sendMsgToMembers(JSON.stringify([2, [socket.DTData.id]]), socket);
 }
 
 function handleCommand(command, socket) {
@@ -65,79 +76,41 @@ function handleCommand(command, socket) {
             var newLobbyID = 10000 + Math.floor(Math.random() * 90000);
 
             while (true) { // Checks if generated newLobbyID is duplicate
-                var foundDuplicate = false;
-
-                for (var i = 0; i < lobbies.length; i++) {
-                    if (lobbies[i].id === newLobbyID) {
-                        newLobbyID = 10000 + Math.floor(Math.random() * 90000);
-                        foundDuplicate = true;
-                        break;
-                    }
-                }
-
-                if (!foundDuplicate) break;
-            }
-
-            var newLobby = new Lobby(newLobbyID, command[1], command[2], command[3]);
-
-            lobbies.push(newLobby);
-            socket.DTData.lobbyID = newLobbyID;
-            sendJoinLobbyInstruction(socket, newLobby);
-        } else if (command[0] === 1) { // Join lobby
-            for (var i = 0; i < lobbies.length; i++) {
-                if (lobbies[i].id === command[1]) {
-                    socket.DTData.lobbyID = command[1];
-                    sendJoinLobbyInstruction(socket, lobbies[i]);
-
-                    return;
-                }
-            }
-
-            socket.send(JSON.stringify([1]));
-        } else if (command[0] === 10 || command[0] === 11 || command[0] === 12) { // Draw line, Erase line, Brush line
-            for (var i = 0; i < lobbies.length; ++i) {
-                if (lobbies[i].id === socket.DTData.lobbyID) {
-                    lobbies[i].instructions.push(command);
+                if (Lobby.getLobbyByID(newLobbyID)) {
+                    newLobbyID = 10000 + Math.floor(Math.random() * 90000);
+                } else {
                     break;
                 }
             }
-            for (var sckt in sockets) {
-                if (sockets[sckt].DTData.lobbyID === socket.DTData.lobbyID) {
-                    sockets[sckt].send(JSON.stringify([0, [command]]));
-                }
+
+            var newLobby = new Lobby(newLobbyID, command[1], command[2], command[3]);
+            lobbies.push(newLobby);
+            socket.DTData.lobbyID = newLobbyID;
+            newLobby.sendJoinInstruction(socket);
+        } else if (command[0] === 1) { // Join lobby
+            var lobby = Lobby.getLobbyByID(command[1]);
+            
+            if (lobby) {
+                socket.DTData.lobbyID = command[1];
+                lobby.sendJoinInstruction(socket);
+            } else {
+                socket.send(JSON.stringify([1]));
             }
+        } else if (command[0] === 10 || command[0] === 11 || command[0] === 12) { // Draw line, Erase line, Brush line
+            var lobby = Lobby.getLobbyByID(socket.DTData.lobbyID);
+            
+            lobby.instructions.push(command);
+            lobby.sendMsgToMembers(JSON.stringify([0, [command]]));
         } else if (command[0] === 9) { // Player update
-            for (var sckt in sockets) {
-                if (sockets[sckt].DTData.lobbyID === socket.DTData.lobbyID && sockets[sckt] !== socket) {
-                    sockets[sckt].send(JSON.stringify([1, [socket.DTData.id,
-                                                      command[1], command[2],
-                                                      command[3], command[4],
-                                                      command[5]]]));
-                }
-            }
+            Lobby.getLobbyByID(socket.DTData.lobbyID).sendMsgToMembers(JSON.stringify([1, [socket.DTData.id, command[1], command[2], command[3], command[4], command[5]]]), socket);
         } else if (command[0] === 50) { // New color
-          for (var i = 0; i < lobbies.length; ++i) {
-              if (lobbies[i].id === socket.DTData.lobbyID) {
-                  lobbies[i].palette.unshift(command[1]);
-
-                  for (var j = 1; j < lobbies[i].palette.length; ++j) {
-                      if (lobbies[i].palette[j] === command[1]) {
-                          lobbies[i].palette.splice(j, 1);
-                          break;
-                      }
-                  }
-
-                  if (lobbies[i].palette.length > 20) lobbies[i].palette = lobbies[i].palette.slice(0, 20);
-
-                  for (var sckt in sockets) {
-                      if (sockets[sckt].DTData.lobbyID === socket.DTData.lobbyID) {
-                          sockets[sckt].send(JSON.stringify([3, lobbies[i].palette]));
-                      }
-                  }
-
-                  break;
-              }
-          }
+            var lobby = Lobby.getLobbyByID(socket.DTData.lobbyID);
+            
+            lobby.palette.unshift(command[1]);
+            if (lobby.palette.indexOf(command[1]) !== -1) lobby.palette.splice(lobby.palette.indexOf(command[1]), 1);
+            if (lobby.palette.length > 20) lobby.palette = lobby.palette.slice(0, 20);
+            
+            lobby.sendMsgToMembers(JSON.stringify([3, lobby.palette]));
         }
     } catch (e) {
         console.error(e);
