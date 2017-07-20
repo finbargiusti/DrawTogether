94c8ab0cf7debe526e94b0f8dbb068e344bcee5b
David Payr <dpayr@musica-viva.de>
2017-07-20 21:00:02 +0200

    Restructured more code

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -55,10 +55,9 @@ document.addEventListener("mousemove", function(event) {
         y: r(mouseYElement(canvas))
     };
 
-    if (connected && lobbyID) {
+    if (connected && lobbyID) { // Send cursor information
         socket.send(JSON.stringify([9, r(thisPosition.x), r(thisPosition.y), (eyeDropperSelected)?currEyeDropperColor:currColor, getCursorType(), brushSize]));
     }
-
 
     if (mousePressed && !eyeDropperSelected && colorPickContainer.style.display === "none" && currentUI == "draw") {
         if (pencilRadio.checked) { // Draw line
@@ -69,22 +68,19 @@ document.addEventListener("mousemove", function(event) {
             socket.send(JSON.stringify([12, r(lastPosition.x), r(lastPosition.y), r(thisPosition.x), r(thisPosition.y), currColor, brushSize]));
         }
 
-        if (usingNewColor && (pencilRadio.checked || brushRadio.checked)) {
+        if (usingNewColor && (pencilRadio.checked || brushRadio.checked)) { // Send new color for palette update
             socket.send(JSON.stringify([50, currColor]));
             usingNewColor = false;
         }
     }
 
-    if (eyeDropperSelected) {
+    if (eyeDropperSelected) { // Update eyedropper's color based on pixel below it
         let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
         let currentIndex = (Math.clamp(thisPosition.x, 0, canvas.width)*4)+Math.clamp(thisPosition.y, 0, canvas.height)*canvas.width*4;
         currEyeDropperColor = "rgb("+imageData.data[currentIndex] +","+ imageData.data[currentIndex+1] + "," + imageData.data[currentIndex+2] + ")"
     }
 
     lastPosition.x = mouseXElement(canvas), lastPosition.y = mouseYElement(canvas);
-});
-sizeSlider.addEventListener("change", function() {
-    brushSize = Number(sizeSlider.value);
 });
 document.addEventListener("mousedown", function() {
     if (eyeDropperSelected) {
@@ -102,25 +98,7 @@ document.addEventListener("mousedown", function() {
 document.addEventListener("mouseup", function() {
     mousePressed = false;
 });
-createLobbyBtn.addEventListener("click", function() {
-    optionPanel.style.display = "block";
-    optionPanel.getElementsByClassName("popup")[0].style.left = "50%";
-    optionPanel.getElementsByClassName("popup")[0].style.top = "50%";
-    validationClock = setInterval(validateCreateLobby, 0);
-});
-createLobbyConfirm.addEventListener("click", function() {
-    if (validateCreateLobby()) {
-        createLobbyRequest(parseInt(createLobbyWidth.value), parseInt(createLobbyHeight.value), createLobbyBgColor.value);
-    }
-});
-submitIDBtn.addEventListener("click", function() {
-    joinLobbyRequest(Number(lobbyIDInput.value));
-});
-createLobbyBgColor.addEventListener("keydown", function() {
-    setTimeout(function() {
-        createLobbyBgColor.style.borderColor = createLobbyBgColor.value;
-    }, 16);
-});
+// Makes popups be closable
 let popupCloseButtons = document.getElementsByClassName("fa fa-window-close fa-lg");
 for (let i = 0; i < popupCloseButtons.length; i++) {
     popupCloseButtons[i].addEventListener("click", function() {
@@ -130,27 +108,3 @@ for (let i = 0; i < popupCloseButtons.length; i++) {
 createLobbyClose.addEventListener("click", function() {
     clearInterval(validationClock);
 });
-closeColorPickButton.addEventListener("click", function() {
-    clearInterval(colorUpdateClock);
-    openColorPickButton.style.backgroundColor = currColor;
-});
-openColorPickButton.addEventListener("click", function() {
-    colorUpdateClock = setInterval(colorUpdate);
-    colorPickContainer.style.display = "block";
-});
-
-function validateCreateLobby() {
-    if (Number(createLobbyWidth.value) >= 256 && Number(createLobbyHeight.value) >= 256 && Number(createLobbyWidth.value) <= 16384 && Number(createLobbyHeight.value) <= 16384) {
-        createLobbyConfirm.disabled = false;
-        createLobbyWidth.style.background = "white";
-        createLobbyHeight.style.background = "white";
-        createLobbyConfirm.style.opacity = 1;
-        return true;
-    } else {
-        createLobbyConfirm.disabled = true;
-        createLobbyWidth.style.background = "red";
-        createLobbyHeight.style.background = "red";
-        createLobbyConfirm.style.opacity = 0.333;
-        return false;
-    }
-}
