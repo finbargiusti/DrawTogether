15fac294f5b3b2d9e587a016f95e5c0be5f8314c
finbargiusti <finbargiusti@gmail.com>
2019-03-12 18:25:12 +0000

    Add checkbox functionality to Input

--- src/client/src/elems/Input.js
+++ src/client/src/elems/Input.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Input.css";
 
-const Input = ({ changeValue, type, propsObject }) => {
+const Input = ({ changeValue, propsObject, label }) => {
+  const [checkboxState, setCheckboxState] = useState(false);
+
   const props = {
     onChange: e => {
       changeValue(e.target.value);
     },
-    className: "input"
+    className: "input",
+    id: { label }
   };
 
-  return <input {...Object.assign(props, propsObject)} />;
+  return (
+    <label
+      className="inputWrapper"
+      onMouseDown={() => {
+        setCheckboxState(!checkboxState);
+        console.log(checkboxState);
+      }}
+    >
+      {propsObject.type === "checkbox" && (
+        <span
+          className={"inputWrapper__icon " + (checkboxState && "toggled")}
+        />
+      )}
+      {label && <p className="inputWrapper__label">{label}</p>}
+      <input {...Object.assign(props, propsObject)} />
+    </label>
+  );
 };
 
 export default Input;
