3c96aed2732de6f54c77ffa772a142162ca7bb56
finbargiusti <finbargiusti@gmail.com>
2023-02-24 12:19:08 +0000

    add accordion for toolbox

--- src/lib/Tools.svelte
+++ src/lib/Tools.svelte
@@ -10,9 +10,13 @@
   import BrushPicker from './BrushPicker.svelte';
   import Recorder from './Recorder.svelte';
 
+  import {drawing} from "../logic/state";
+
   let toolbox: HTMLDivElement;
 
   let active = false;
+
+  let open = true;
 
   onMount(() => {
     interact(toolbox).dropzone({
@@ -33,33 +37,65 @@
 </script>
 
 <div class={'tools' + (active ? ' active' : '')} bind:this={toolbox}>
-  <Draggable titleName="Lobby ID">
-    <LobbyId />
-  </Draggable>
-  <Draggable titleName="Player List">
-    <PlayerList />
-  </Draggable>
-  <Draggable titleName="Messages">
-    <ChatBox />
-  </Draggable>
-  <Draggable titleName="Brush Picker">
-    <BrushPicker />
-  </Draggable>
-  <Draggable titleName="Recorder">
-    <Recorder />
-  </Draggable>
+  <div class="tool-wrap {!open && 'inactive'}">
+    <Draggable titleName="Lobby ID">
+      <LobbyId />
+    </Draggable>
+    <Draggable titleName="Player List">
+      <PlayerList />
+    </Draggable>
+    <Draggable titleName="Messages">
+      <ChatBox />
+    </Draggable>
+    <Draggable titleName="Brush Picker">
+      <BrushPicker />
+    </Draggable>
+    <Draggable titleName="Recorder">
+      <Recorder />
+    </Draggable>
+  </div>
+  <button id="toolbox-open {$drawing && 'drawing'}" on:click={() => open = !open}>⬇</button>
 </div>
 
-<style lang="sass">
-.tools
-  grid-area: tools
-  display: flex
-  flex-direction: column
-  align-items: stretch
-  justify-content: stretch
-  transition: background-color 0.3s
-  gap: 8px
+<style lang="scss">
+.tools {
+  grid-area: tools;
+  transition: background-color 0.4s;
 
-  &.active
-    background-color: rgba(255, 255, 255, 0.1)
+  .tool-wrap {
+    overflow: hidden;
+    margin-top: 0px; 
+
+    max-height: calc(100vh - 50px);
+
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    justify-content: stretch;
+    transition: max-height 0.4s;
+    gap: 8px;
+    margin-bottom: 10px;
+
+    &.inactive {
+      max-height: 0px; 
+      margin-bottom: 0px;
+    }
+  }
+
+  #toolbox-open {
+    pointer-events: all;
+    height: 40px;
+    width: 100%;
+    transition: opacity 0.4s;
+
+    &.drawing {
+      pointer-events: none;
+      opacity: 0;
+    }
+  }
+
+  &.active {
+    background-color: rgba(120, 120, 120, 0.3);
+  }
+}
 </style>
